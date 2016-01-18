8c36d968c1650e45bfdc26d91c691e61b963bcdf
Juan Castañeda <mko@itedia.mx>
2016-01-18 12:09:54 -0600

    BD: guardar cambios

--- moduloDAP/modelo/modPersonalAgregarModificar.php
+++ moduloDAP/modelo/modPersonalAgregarModificar.php
@@ -6,6 +6,12 @@
 
 	$objPersonal = new clsPersonal();
 
+	/*$_POST['idEstimulo'] = 977;
+	$_POST['idPersonal'] = 25;
+	$_POST['gradoAcademico'] = 0;
+	$_POST['puestoDrectivo'] = 1;
+	$_POST['asistencias'] = 23.5;*/
+
 	$objPersonal->setidEstimulo($_POST['idEstimulo']);
 	$objPersonal->setgradoAcademico($_POST['gradoAcademico']);
 	$objPersonal->setpuestoDrectivo($_POST['puestoDrectivo']);
